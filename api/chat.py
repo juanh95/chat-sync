8bd0a3a4b107a8e7663c557f9eaa9e753015ee92
Juan Hernandez <hernandezjuanm39@gmail.com>
2024-06-05 03:15:29 +0000

    Protected the Chat route

--- api/chat.py
+++ api/chat.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import os
 from schemas.chat_message import ChatMessage
 from typing import Dict, List
+from utils.utils import get_current_user_from_jwt
+from jose import JWTError
 
 load_dotenv()
 
@@ -20,8 +22,12 @@ class ConnectionManager:
       self.active_connections[websocket] = username
 
    def disconnect(self, websocket: WebSocket):
-      return self.active_connections.pop(websocket)
-
+      if websocket in self.active_connections:
+         return self.active_connections.pop(websocket)
+      else:
+         print("Websocket not found in active connections")
+         return None
+         
    async def send_room_message(self, chat_message_data: ChatMessage):
       for connection in self.active_connections.keys():
          await connection.send_json(chat_message_data.model_dump())
@@ -34,32 +40,53 @@ manager = ConnectionManager()
 
 @router.websocket("/chat/{username}")
 async def web_socket_endpoint(websocket: WebSocket, username: str):
-   
-   # token = websocket.headers.get("Authorization")
-   
-   # if not token or not token.startswith("Bearer"):
-   #    raise WebSocketException(code=4001, reason="Invalid Token")
-   
-   # token = token[7:] # strip off the "Bearer" prefix
-
-   # username = await get_current_user_from_jwt(token)
-
-   # if not username:
-   #    raise WebSocketException(code=4001, reason="Invalid Authentication Credentials")
-   
    try:
-      await manager.connect(websocket, username)
+      await websocket.accept()
+      print("Accepted the connection")
+
+      authenticated = False
       
-      while True:
-         # TODO: Handle when data isn't received this way
-         data = await websocket.receive_json()
-         chat_message_data = ChatMessage(**data)
+      print("Waiting to receive initial JSON")
+      data = await websocket.receive_json()
+
+      token = data.get("token")
+
+      if not token: 
+         raise WebSocketDisconnect(code=status.WS_1008_POLICY_VIOLATION, reason="Missing authentication token") 
+
+      print("Token was present")
+      
+      try: 
+         print("About to verify JWT")
+         sender_username = await get_current_user_from_jwt(token)
+         print(f"Done verifying JWT, the sender_username is {sender_username}")
+
+         if sender_username != username:
+            print("----- Got into the if evaluation")
+            websocket.close(code=status.WS_1008_POLICY_VIOLATION, reason="Invalid Credentials")
          
-         await manager.send_room_message(chat_message_data)
+         authenticated = True
+
+         print("Token was validated")
+
+      except JWTError or WebSocketDisconnect:
+         websocket.close(code=status.WS_1008_POLICY_VIOLATION, reason="Invalid Authentication")
+      
+      if authenticated: 
+         while True:
+            print("Regular chat in progress")
+            data = await websocket.receive_json()
+            chat_message_data = ChatMessage(**data)
+            
+            await manager.send_room_message(chat_message_data)
+      else: 
+         await websocket.send_json({"message": "Authentication failed"})
+         await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
    
    except WebSocketDisconnect: 
       
       disconnected_username = manager.disconnect(websocket)
 
-      await manager.broadcast(f"{disconnected_username} left the chat")
+      if disconnected_username != None:
+         await manager.broadcast(f"{disconnected_username} left the chat")
    
